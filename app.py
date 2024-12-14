838b4dfceab9443acf2d4d3f0ea7c2276badfd3d
Abdallah Saleh El-Gholmy <58488787+engasaleh@users.noreply.github.com>
2024-12-14 06:41:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
 def home():
-    return "Hello, OpenShift!"
+    return "<h1>Hello, OpenShift!</h1><h2>Hello, Abdallah Our new DevOps Engineer!</h2>"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
